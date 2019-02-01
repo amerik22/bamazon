bab49167982d4d29e83ccc8463224223f8f47405
Andrea <andrea.erikson@gmail.com>
2019-01-31 18:28:58 -0700

    updated js file

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -9,28 +9,33 @@ var connection = mysql.createConnection({
     database: "bamazon_db"
 });
 
-connection.connect(function(err){
+connection.connect(function(err) {
     if (err) throw err;
-    console.log("connected as id " + connection.threadId)
     queryProducts();
-    buyProduct();
-});
 
-
+  });
 
 function queryProducts(){
-    console.log("Welcome to BAMAZON");
-    console.log("-----------------------------------");
+
     connection.query("SELECT * FROM products", function(err, res){
+        if(err) throw err;
+        console.log("Welcome to BAMAZON");
+        console.log("-----------------------------------");
         for (var i = 0; i < res.length; i++){
             console.log(res[i].id + " | " + res[i].product_name + " | " + res[i].department_name + " | " + res[i].price)
         }
         console.log("-----------------------------------");
-    })
-}
 
-function buyProduct(){
-    inquirer.prompt(
+        buyProduct(res);
+    });
+        console.log(" ");
+        
+    }
+
+
+
+function buyProduct(res){
+   inquirer.prompt(
         [{
             type: "input",
             name: "choose_id",
@@ -47,16 +52,105 @@ function buyProduct(){
     )
     .then(function(user){
         connection.query("SELECT * FROM products WHERE id=?", user.choose_id, function(err, res){
+            //console.log(res);
             for (var i = 0; i < res.length; i++){
                 if (user.choose_qty < res[i].stock_quantity){
                     console.log("-----------------------------------");
                     console.log("That item is in stock ");
-                    console.log("Your total is: " + res[i].price * user.choose_qty)
+                    console.log("Your total is: " + res[i].price * user.choose_qty + " dollars");
+                    console.log("-----------------------------------");
+                    var updateStock = (res[i].stock_quantity - user.choose_qty);
+                    var purchaseId = user.choose_id;
+                    //console.log(updateStock, purchaseId);
                 }
+
+                else {
+                    console.log("-----------------------------------");
+                    console.log("Sorry that item is not in stock");
+                    console.log("-----------------------------------");
+                    connection.end();
+                }
+
+                
+                
             }
-        } )
-    
-        
-    })
+            confirmPurchase(updateStock, purchaseId);
+            console.log(updateStock, purchaseId);
+            //confirmPurchase(user.choose_qty, user.choose_id);
+            //console.log(user.choose_qty, user.choose_id);
+        });  
+       
+    });
+
 }
 
+    function confirmPurchase(updateStock, purchaseId){
+        inquirer.prompt(
+            [{
+                type: "confirm",
+                name: "confirmPurchase",
+                message: "Do you want to make this purchase?"
+            }]
+        )
+        .then(function(updateStock, purchaseId){
+            console.log(updateStock, purchaseId)
+            connection.query("UPDATE products SET ? WHERE ?",
+            [
+                {
+                    stock_quantity: updateStock
+                },
+                {
+                    id: purchaseId
+                }
+            ],
+            function(error){
+                if (error) throw err;
+                console.log("Thank you for your purchase");
+                connection.end();
+            });               
+        })
+    }
+
+
+/*function confirmPurchase(updateStock, purchaseId){
+    
+    inquirer.prompt(
+        [{
+            type: "confirm",
+            name: "confirmPurchase",
+            message: "Do you want to make this purchase?"
+        }]
+    )
+    .then(function(updateStock, purchaseId){
+        connection.query("UPDATE products SET ? WHERE ?", 
+        [
+           {
+               stock_quantity: updateStock
+           }, 
+           {
+               id: purchaseId
+           }
+        ],function(error){
+            if (error) throw err;
+            console.log("Thank you for your purchase");
+        }
+      }
+    }
+    
+    
+
+
+   
+
+/*function confirmPurchase(qty, id){
+   //console.log(qty, id);
+    inquirer.prompt(
+        [{
+            type: "confirm",
+            name: "confirmPurchase",
+            message: "Do you want to make this purchase?"
+        }]
+    )
+    .then(function(qty, id){
+
+    }*/
--- schema.sql
+++ schema.sql
@@ -13,24 +13,3 @@ stock_quantity INT default 0,
 PRIMARY KEY (id)
 );
 
--- Insert a set of records.
-INSERT INTO products (product_name, department_name, price, stock_quantity) 
-VALUES ("blenders", "kitchen", 50.00, 100);
-INSERT INTO products (product_name, department_name, price, stock_quantity) 
-VALUES ("marshmallows", "food", 2.00, 10000);
-INSERT INTO products (product_name, department_name, price, stock_quantity) 
-VALUES ("flashlight", "tools", 10.00, 5000);
-INSERT INTO products (product_name, department_name, price, stock_quantity) 
-VALUES ("shower gel", "bath", 3.50, 15000);
-INSERT INTO products (product_name, department_name, price, stock_quantity) 
-VALUES ("dvd player", "electronics", 35.00, 500);
-INSERT INTO products (product_name, department_name, price, stock_quantity) 
-VALUES ("toaster oven", "kitchen", 42.00, 300);
-INSERT INTO products (product_name, department_name, price, stock_quantity) 
-VALUES ("granola bars", "food", 1.25, 4000);
-INSERT INTO products (product_name, department_name, price, stock_quantity) 
-VALUES ("hammer", "tools", 15.00, 400);
-INSERT INTO products (product_name, department_name, price, stock_quantity) 
-VALUES ("loofah", "bath", 9.00, 10000);
-INSERT INTO products (product_name, department_name, price, stock_quantity) 
-VALUES ("headphones", "electronics", 85.00, 100);
